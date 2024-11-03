bf74ae39a2d4a6d261a6da32935b7257905fedfa
martinjohnm <johnmmartin74@gmail.com>
2024-11-03 16:52:13 +0530

    intital setup with index and passport

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,19 +1,49 @@
+import { WebSocketServer } from "ws";
+import express from "express";
+import { configDotenv } from "dotenv";
+import cookieParser from "cookie-parser";
+import session from "express-session"
+import { COOKIE_MAX_AGE } from "./consts";
+import cors from "cors"
+import { initPassport } from "./passport";
+import passport from "passport";
+import authRoute from "./router/auth"
 
+const app = express();
+configDotenv()
 
-
-import express from "express"
-
-import { BE_URL } from "@repo/common/config"
-import db from "@repo/db/client"
-
-const app = express()
-
-console.log(BE_URL);
-
-app.get("/", (req, res) => {
-    res.json({
-        message :"hello world"
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET || "cookie-secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure : false, maxAge : COOKIE_MAX_AGE }
     })
-})
+);
 
-app.listen(3000)
+initPassport();
+app.use(passport.initialize());
+app.use(passport.authenticate('session'));
+
+const allowedHosts = process.env.ALLOWED_HOSTS
+  ? process.env.ALLOWED_HOSTS.split(',')
+  : [];
+
+app.use(
+    cors({
+        origin : allowedHosts,
+        methods: 'GET,POST,PUT,DELETE',
+        credentials: true
+    })
+);
+
+
+app.use('/auth', authRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
+
---
+++ apps/backend/src/passport.ts
@@ -0,0 +1,77 @@
+
+
+import passport from 'passport';
+import dotenv from 'dotenv';
+import db from "@repo/db/client"
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
+
+dotenv.config();
+const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID || 'your_google_client_id';
+const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET || 'your_google_client_secret';
+
+
+
+export function initPassport() {
+  if (
+    !GOOGLE_CLIENT_ID ||
+    !GOOGLE_CLIENT_SECRET 
+  ) {
+    throw new Error(
+      'Missing environment variables for authentication providers',
+    );
+  }
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+      },
+      async function (
+        accessToken: string,
+        refreshToken: string,
+        profile: any,
+        done: (error: any, user?: any) => void,
+      ) {
+        const user = await db.user.upsert({
+          create: {
+            email: profile.emails[0].value,
+            name: profile.displayName,
+            // provider: 'GOOGLE',
+          },
+          update: {
+            name: profile.displayName,
+          },
+          where: {
+            email: profile.emails[0].value,
+          },
+        });
+
+        done(null, user);
+      },
+    ),
+  );
+
+  passport.serializeUser(function (user: any, cb) {
+    process.nextTick(function () {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture,
+      });
+    });
+  });
+
+  passport.deserializeUser(function (user: any, cb) {
+    process.nextTick(function () {
+      return cb(null, user);
+    });
+  });
+
+}
