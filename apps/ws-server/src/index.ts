4616559361bae36283233ee7c4056af13cc6dc6d
martinjohnm <johnmmartin74@gmail.com>
2024-11-07 22:50:13 +0530

    added websocket token log

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -15,7 +15,7 @@ wss.on("connection", function connection(ws, req) {
     //@ts-ignore
     const token: string = url.parse(req.url, true).query.token
     const user = extractAuthUser(token, ws);
-    console.log(user);
+    console.log(user.isGuest, user.name, user.userId,);
     
     gameManager.addUser(ws)
 })
