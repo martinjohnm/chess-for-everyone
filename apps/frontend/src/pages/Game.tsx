449a685c6675cc0d12e05e46692ddc0b99b52d28
martinjohnm <johnmmartin74@gmail.com>
2024-11-12 13:47:47 +0530

    if single player joined game added message from ws creatd

--- apps/frontend/src/pages/Game.tsx
+++ apps/frontend/src/pages/Game.tsx
@@ -2,7 +2,7 @@ import { useUser } from "@repo/store/useUser"
 import { useSocket } from "../hooks/useSocket";
 import { useEffect, useState } from "react";
 import { Chess } from "chess.js";
-import { GAME_OVER, INIT_GAME, JOIN_ROOM, MOVE } from "@repo/common/messages";
+import { GAME_ADDED, GAME_OVER, INIT_GAME, JOIN_ROOM, MOVE } from "@repo/common/messages";
 import { ChessBoard } from "../components/ChessBoard";
 
 import { Button } from "@repo/ui/button";
@@ -18,9 +18,10 @@ export const Game = () => {
 
     const [chess, _ ] = useState(new Chess())
     const [board, setBoard] = useState(chess.board());
-
-
     const [started, setStarted] = useState<Boolean>(false)
+    const [added, setAdded] = useState<Boolean>(false)
+    const [gameIDFromSocket, setGameIdFromSocket] = useState<string>("")
+
 
     useEffect(() => {
         if (!user) {
@@ -35,6 +36,10 @@ export const Game = () => {
         socket.onmessage = (event) => {
             const message = JSON.parse(event.data);
             switch (message.type) {
+                case GAME_ADDED: 
+                    setAdded(true)
+                    setGameIdFromSocket(message.gameId)
+                    break
                 case INIT_GAME:
                     setBoard(chess.board())
                     console.log("Game Initialized");
@@ -72,14 +77,30 @@ export const Game = () => {
             </div>
             <div className="md:col-span-1 flex justify-center rounded-lg bg-slate-300 dark:bg-[#262522]">
                 <div className="w-full h-full flex justify-center items-center">
-                    {!started ? (<Button className="bg-[#81b64c] p-4 mt-4 rounded-md w-2/3" onclick={() => {
-                        socket.send(JSON.stringify({
-                            type : INIT_GAME
-                        }))
-                        setStarted(true)
-                    }}>Play</Button>
+                    {!started ? (
+                        <div className="w-full justify-center flex">
+                            {added ? (
+                                <div>
+
+                                </div>
+                            ) : (
+                                
+                                gameId === "random" && (
+                                <Button className="bg-[#81b64c] p-4 mt-4 rounded-md w-2/3" onclick={() => {
+                                    socket.send(JSON.stringify({
+                                        type : INIT_GAME
+                                    }))
+                                    setStarted(true)
+                                }}>Play</Button>
+                                )
+                            
+                            )}
+                            
+                        </div>
                     ) : (
-                        <ExitGame/>
+                        <div className="w-full justify-center flex">
+                            <ExitGame/>
+                        </div>
                     )}
                     
                 </div>
