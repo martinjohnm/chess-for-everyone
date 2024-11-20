cc4c9b3aefe6c7ab558a56d187f1cdd13cbb73d2
martinjohnm <johnmmartin74@gmail.com>
2024-11-20 13:43:25 +0530

    in ws gameManager user reomval logic upgraded

--- apps/ws-server/src/GameManager.ts
+++ apps/ws-server/src/GameManager.ts
@@ -32,7 +32,7 @@ export class GameManager {
       this.users = this.users.filter((user) => user.socket !== socket)
       // if a user initiates a game and then suddenly leave there should be a pendinggameId without any 
       // users so we need to delete that before sending socket message
-      if (this.users.length === 0 ) {
+      if (this.users.length %2 == 0 ) {
         this.pendingGameId = null
       }
       
